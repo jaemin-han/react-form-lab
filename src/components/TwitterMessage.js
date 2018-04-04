26b7d5a2a2e94eb8f37bfa2e870c00243e31714d
jaemin.han <jaemin.han@nyu.edu>
2018-04-04 16:21:39 -0400

    complete but redo

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,17 +4,43 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value:'',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+    });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input 
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+           />
+          
+        <span>
+          {this.props.maxChars - this.state.value.length}
+        </span>
       </div>
     );
   }
 }
+
+TwitterMessage.prototypes = {
+  maxChars: React.PropTypes.number,
+};
+
+TwitterMessage.defaultProps = {
+  maxChars: 140,
+};
 
 export default TwitterMessage;
